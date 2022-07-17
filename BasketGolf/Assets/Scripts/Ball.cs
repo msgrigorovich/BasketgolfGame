dda9eb45eb74348c360093c490052c64c91c82e4
elyasn0 <ilyadems0@gmail.com>
2022-07-17 18:07:45 +0300

    ball physic

--- BasketGolf/Assets/Scripts/Ball.cs
+++ BasketGolf/Assets/Scripts/Ball.cs
@@ -1,13 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
-    public GameObject ball;
+    public float force = 1000f;
 
-    private void OnMouseDown()
+    private Vector2 startPostition;
+    private Vector2 endPosition;
+
+    private Scene sceneMain;
+    private PhysicsScene sceneMainPhysic;
+
+    private Scene scenePrediction;
+    private PhysicsScene scenePredictionPhysic;
+
+    void Start()
     {
-        ball.transform.position = new Vector3(0, 10, 0)*Time.deltaTime;
+        sceneMain = SceneManager.CreateScene("MainScene");
+        sceneMainPhysic = sceneMain.GetPhysicsScene();
+
+        scenePrediction = SceneManager.CreateScene("scenePrediction");
+        scenePredictionPhysic = scenePrediction.GetPhysicsScene();
+
+
     }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            startPostition = GetMousePosition();
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            endPosition = GetMousePosition();
+            Vector2 power = startPostition - endPosition;
+            GetComponent<Rigidbody>().AddForce(power*force, ForceMode.Force);
+            Debug.Log(power);
+        }
+    }
+
+    private Vector2 GetMousePosition() {
+        return Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    }
+
 }
